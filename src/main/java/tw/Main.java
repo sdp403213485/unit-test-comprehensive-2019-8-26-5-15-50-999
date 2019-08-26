7d28f59837d5a7ed10c748366a8b0ba2305036af
sdp <403213485@qq.com>
2019-08-26 20:56:23 +0800

    输输成功

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -1,5 +1,10 @@
 package tw;
 
+import javax.print.attribute.standard.OutputDeviceAssigned;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.omg.PortableServer.ID_ASSIGNMENT_POLICY_ID;
+
 import tw.commands.GuessInputCommand;
 
 /**
@@ -8,11 +13,44 @@ import tw.commands.GuessInputCommand;
 public class Main {
 
     private static final int RUN_TIMES = 6;
-
+    static String str="1234";
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < RUN_TIMES; i++) {
-            String input = new GuessInputCommand().input();
-            System.out.println(input);
+        String result = "";
+    	for (int i = 0; i < RUN_TIMES; i++) {
+            String input = new GuessInputCommand().input();   
+            result=output(input,str);
+            System.out.println(result);
+        }     
+        	
+//            System.out.println(input);
         }
+    
+    
+	public static String output(String input,String str) {
+		if(input.equals(str)) {
+			return "right";
+		}
+		else {
+			int A=0;
+			int B=0;
+			char [] stringInput = input.toCharArray();
+			char [] stringStr = str.toCharArray();
+			for(int i=0;i<4;i++) {
+				for(int j=0;j<4;j++) {
+					if(stringInput[i]==stringStr[j] && i==j) {
+						A=A+1;
+					}else if(stringInput[i]==stringStr[j] && i!=j) {
+						B=B+1;
+					}
+				}
+			}
+			return A+"A"+B+"B";
+		}
     }
 }
+    
+
+
+
+    
+
